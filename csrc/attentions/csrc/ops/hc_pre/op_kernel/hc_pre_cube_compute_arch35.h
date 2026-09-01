5027081e90b2e70d643f961ec166fa9482c9947a
AndyLi429 <AndyLi429@noreply.gitcode.com>
2026-09-01 12:29:41 +0800

    fix(hc-pre): use IEEE FP32 projection

--- csrc/attentions/csrc/ops/hc_pre/op_kernel/hc_pre_cube_compute_arch35.h
+++ csrc/attentions/csrc/ops/hc_pre/op_kernel/hc_pre_cube_compute_arch35.h
@@ -84,9 +84,8 @@ namespace HcPreNs
             AscendC::SetFlag<AscendC::HardEvent::M_MTE1>(3);
             AscendC::SetFlag<AscendC::HardEvent::M_MTE1>(4);
 
-            // hf32 compute
-            AscendC::SetHF32Mode(1);
-            AscendC::SetHF32TransMode(1);
+            AscendC::SetHF32Mode(0);
+            AscendC::SetHF32TransMode(0);
         }
 
         __aicore__ inline void CopyInB1Nd2Nz(uint64_t k, uint64_t currentK, uint64_t baseN, const AscendC::GlobalTensor<float> &bGlobal,
@@ -235,6 +234,7 @@ namespace HcPreNs
             AscendC::WaitFlag<AscendC::HardEvent::M_MTE1>(3);
             AscendC::WaitFlag<AscendC::HardEvent::M_MTE1>(4);
             AscendC::SetHF32Mode(0);
+            AscendC::SetHF32TransMode(0);
         }
     };
 }
